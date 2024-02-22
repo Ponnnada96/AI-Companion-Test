de2d2ee19886089ed49bb68a528dd289068a5dd4
PrasadPonnada <PrasadPonnada09@gmail.com>
2024-02-22 18:38:11 +0530

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-import { ErrorBoundary } from "react-error-boundary";
 
-
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-poppins'
-})
 
 export const metadata: Metadata = {
   title: "companion.ai",
@@ -25,11 +17,9 @@ export default function RootLayout({
 }>) {
   return (
 
-    <html lang="en" suppressHydrationWarning>
-      <body className={poppins.className}>
-    
+    <html lang="en">
+      <body>
             {children}
-      
       </body>
     </html>
 
